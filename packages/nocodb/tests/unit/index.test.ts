5b3b4052f7c37f90f2cd393176f0ffca8730b40c
Fendy Heryanto <fendy@nocodb.com>
2025-12-20 13:49:28 +0000

    add unit test, fix error

--- packages/nocodb/tests/unit/index.test.ts
+++ packages/nocodb/tests/unit/index.test.ts
@@ -8,6 +8,9 @@ import { dataApiV3Test } from './rest/tests/dataApiV3/index.test';
 import { processorTests } from './processor/index.test';
 import { errorTests } from './error/index.test';
 import { rollupTests } from './rollup/index.test';
+import { linksTests } from './links/index.test';
+import { dbQueryClientTests } from './dbQueryClient/index.test';
+import { rateLimitUtilTests } from './rate-limit-utils/index.test';
 
 process.env.NODE_ENV = 'test';
 process.env.TEST = 'true';
@@ -28,6 +31,7 @@ dotenv.config({
   errorTests();
   restTests();
   processorTests();
+  rateLimitUtilTests();
   await dataApiV3Test();
 
   run();
